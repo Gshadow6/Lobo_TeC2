3725892ecd2d551313f443da5e462e30d2e6a5eb
axelshadow <axelshadow7@gmail.com>
2023-02-10 11:43:32 -0600

    Creacion de clase Automovil

---
+++ src/automovil/Automovil.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automovil;
+
+/**
+ *
+ * @author AXEL
+ */
+public class Automovil {
+    String marca;
+    String submarca;
+    int anio;
+    int kilometraje;
+    String transmision;
+
+    public Automovil(String marca, String submarca, int anio, int kilometraje, String transmision) {
+        this.marca = marca;
+        this.submarca = submarca;
+        this.anio = anio;
+        this.kilometraje = kilometraje;
+        this.transmision = transmision;
+    }
+    
+    public Automovil(){
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSubmarca() {
+        return submarca;
+    }
+
+    public void setSubmarca(String submarca) {
+        this.submarca = submarca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(int kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+
+    @Override
+    public String toString() {
+        return "El Automovil de marca " + marca + ", con submarca " + submarca + ", año " + anio + ", con " + kilometraje + " km, y de transmision " + transmision;
+    }
+    
+    
+}
